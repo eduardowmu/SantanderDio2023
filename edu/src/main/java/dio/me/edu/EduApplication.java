41aba79581734336ab1fd52f26110a2cf1e63654
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2023-10-22 23:45:04 -0300

    para entrega

--- edu/src/main/java/dio/me/edu/EduApplication.java
+++ edu/src/main/java/dio/me/edu/EduApplication.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//https://github.com/falvojr/santander-dev-week-2023
+//https://github.com/digitalinnovationone/santander-dev-week-2023-api
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class EduApplication {
